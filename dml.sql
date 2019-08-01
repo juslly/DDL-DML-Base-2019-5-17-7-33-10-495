1c978dec665dd9e14f07316923be9b5153b1d5dd
juslly <30677857+juslly@users.noreply.github.com>
2019-08-01 18:01:07 +0800

    update dml.sql
    具体的sql语句

--- dml.sql
+++ dml.sql
@@ -1,7 +1,9 @@
--- 插入记录  
+-- 插入记录 
+insert into 表名（列名，.....） values( 值1，.....);
 
 -- 修改记录  
-
+update 表名 set 列=新值，列=新值 where 条件
 --  删除记录  
-
+delete from 表名 where 筛选条件；
 -- 查询记录  
+select * from 表名 where 条件
